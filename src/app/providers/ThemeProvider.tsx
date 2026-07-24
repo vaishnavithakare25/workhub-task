1ec54cc122971ce3ba1a081b087046143e112ce8
Vaishnavi Thakare <vaishnavi.thakare@zensar.com>
2026-07-24 10:24:18 +0530

    removed empty files

--- src/app/providers/ThemeProvider.tsx
+++
@@ -1,5 +0,0 @@
-export default function ThemeProvider({ children }: { children: React.ReactNode })
- { 
-    return 
-    <>{children}</> 
-}
--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,2 +1,2 @@
 export * from './QueryProvider';
-export * from './ThemeProvider';
+// export * from './ThemeProvider';
